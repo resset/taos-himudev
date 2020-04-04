d491461ef2ba704de95b029d02078f7ba18adda8
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2020-04-04 19:44:01 +0200

    Disabled uart, it needs being rewrited anyway
    Our current .text is 245740 bytes, we have to change that.

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,6 @@
 #[macro_export]
 macro_rules! print {
     ($($args:tt)+) => {{
-		use core::fmt::Write;
-		let _ = write!(crate::uart::Uart::new(0x1000_0000), $($args)+);
 	}};
 }
 #[macro_export]
@@ -66,80 +64,10 @@ extern "C" fn abort() -> ! {
 // ///////////////////////////////////
 #[no_mangle]
 extern "C" fn kmain() {
-    // Main should initialize all sub-systems and get
-    // ready to start scheduling. The last thing this
-    // should do is start the timer.
-
-    // Let's try using our newly minted UART by initializing it first.
-    // The UART is sitting at MMIO address 0x1000_0000, so for testing
-    // now, lets connect to it and see if we can initialize it and write
-    // to it.
-    let mut uart = uart::Uart::new(0x1000_0000);
-
-    uart.init();
-
-    println!("taos v0.1");
-
-    // Now see if we can read stuff:
-    // Usually we can use #[test] modules in Rust, but it would convolute the
-    // task at hand. So, we'll just add testing snippets.
     loop {
-        if let Some(byte) = uart.get() {
-            //use numtoa::NumToA;
-            //let mut buffer = [0u8; 3];
-            //println!("{}", (byte as u8).numtoa_str(10, &mut buffer));
-            match byte {
-                8 | 127 => {
-                    // This is a backspace, so we essentially have
-                    // to write a space and backup again:
-                    print!("{}{}{}", 8 as char, ' ', 8 as char);
-                }
-                10 | 13 => {
-                    // Newline or carriage-return
-                    println!();
-                }
-                0x1b => {
-                    // Those familiar with ANSI escape sequences
-                    // knows that this is one of them. The next
-                    // thing we should get is the left bracket [
-                    // These are multi-byte sequences, so we can take
-                    // a chance and get from UART ourselves.
-                    // Later, we'll button this up.
-                    if let Some(bracket) = uart.get() {
-                        if bracket == 91 {
-                            // This is a right bracket! We're on our way!
-                            if let Some(esc_cmd) = uart.get() {
-                                match esc_cmd as char {
-                                    'A' => {
-                                        println!("That's the up arrow!");
-                                    }
-                                    'B' => {
-                                        println!("That's the down arrow!");
-                                    }
-                                    'C' => {
-                                        println!("That's the right arrow!");
-                                    }
-                                    'D' => {
-                                        println!("That's the left arrow!");
-                                    }
-                                    _ => {
-                                        println!("That's something else.....");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                _ => {
-                    print!("{}", byte as char);
-                }
-            }
-        }
     }
 }
 
 // ///////////////////////////////////
 // / RUST MODULES
 // ///////////////////////////////////
-
-pub mod uart;
